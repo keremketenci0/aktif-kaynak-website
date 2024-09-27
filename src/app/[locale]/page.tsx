24b5ffacc8530b965c1a2b505280d75b42551973
keremketenci0 <keremketenci0@gmail.com>
2024-09-27 10:08:14 +0300

    test

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,12 @@
-import {useTranslations} from 'next-intl';
-import {Link} from '@/i18n/routing';
- 
+import { useTranslations } from 'next-intl';
+import { Link } from '@/i18n/routing';
+
 export default function Page() {
   const t = useTranslations('Page.Home');
   return (
     <div>
       <h1>{t('title')}</h1>
+      <p>test</p>
       <Link href="/about">{t('about')}</Link>
     </div>
   );
